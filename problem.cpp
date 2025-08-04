658ae836c9e6c96e22d36d2372543762343560f0
suthakaranburaj <suthakaranburaj@gmail.com>
2025-08-04 20:56:01 +0530

    initializing

---
+++ problem.cpp
@@ -0,0 +1,40 @@
+/*
+    author : suthakaranburaj
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(x) (x).begin(),(x).end()
+#define sz(a) (int)(a).size()
+
+/*
+    Happy Coding
+*/
+
+void check_nth_bit(int &n ,int &i){
+  string ans = 
+}
+void solve() {
+    int n,i;
+    cin>>n>>i;
+    check_nth_bit(n,i);
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fastio;
+    
+    int a;
+    cin>>a;
+    while(a--) {
+        solve();
+    }
+    
+    return 0;
+}
---
+++ temp.cpp
@@ -0,0 +1,34 @@
+/*
+    author : suthakaranburaj
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(x) (x).begin(),(x).end()
+#define sz(a) (int)(a).size()
+
+/*
+    Happy Coding
+*/
+void solve() {
+    cout << "Hello" << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fastio;
+    
+    int a;
+    cin>>a;
+    while(a--) {
+        solve();
+    }
+    
+    return 0;
+}
