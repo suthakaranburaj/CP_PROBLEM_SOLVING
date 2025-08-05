0cb0501376be020263fc953fbde93a272de54161
suthakaranburaj <suthakaranburaj@gmail.com>
2025-08-05 15:25:14 +0530

    Prime factorisation of a Number

--- problem.cpp
+++ problem.cpp
@@ -12,14 +12,67 @@ using namespace std;
 /*
     Happy Coding
 */
-void solve() {
-    int a,b;
-    cin>>a>>b;
-    a = a ^ b;
-    b = a ^ b;
-    a = a ^ b;
 
-    cout<<"a:"<<a<<"b:"<<b<<endl;
+// Sieve of Eratosthenes
+vector<bool> find_prime_nums(int max_num) {
+    vector<bool> prime(max_num + 1, true);
+    prime[0] = prime[1] = false;
+
+    for (int i = 2; i * i <= max_num; i++) {
+        if (prime[i]) {
+            for (int j = i * i; j <= max_num; j += i) {
+                prime[j] = false;
+            }
+        }
+    }
+    return prime;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int max_num = *max_element(all(arr));
+    vector<bool> is_prime = find_prime_nums(max_num);
+
+    // Store all primes up to max_num
+    vector<int> primes;
+    for (int i = 2; i <= max_num; i++) {
+        if (is_prime[i]) {
+            primes.push_back(i);
+        }
+    }
+
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n; i++) {
+        int num = arr[i];
+        vector<int> factors;
+        
+        for (int prime : primes) {
+            while (num % prime == 0) {
+                factors.push_back(prime);
+                num /= prime;
+            }
+            if (num == 1) break;
+        }
+        
+        if (num > 1) factors.push_back(num);  // For remaining prime > sqrt(n)
+        ans.push_back(factors);
+    }
+
+    // Print result
+    for (const auto& vec : ans) {
+        cout << "[ ";
+        for (int val : vec) {
+            cout << val << " ";
+        }
+        cout << "]\n";
+    }
 }
 
 int main() {
@@ -30,9 +83,9 @@ int main() {
 #endif
     fastio;
     
-    int T;
-    cin>>T;
-    while(T--) {
+    int t;
+    cin >> t;
+    while(t--) {
         solve();
     }
     return 0;
