de781d053c336b5f35b9b5514333001ae572e38a
suthakaranburaj <suthakaranburaj@gmail.com>
2025-08-06 20:26:32 +0530

    Decoration Discount

--- problem.cpp
+++ problem.cpp
@@ -9,63 +9,42 @@ using namespace std;
 #define all(x) (x).begin(),(x).end()
 #define sz(a) (int)(a).size()
 
-/*
-    Happy Coding
-*/
 void print_1d_array(vector<int>&arr){
     int n = arr.size();
     cout<<"[";
     for(int i = 0;i<n;i++){
-        cout<<arr[i]<<",";
+        cout<<arr[i]<<" ";
     }
     cout<<endl;
 }
 
-vector<int> build_prime_prefix(int max_num) {
-    vector<bool> is_prime(max_num + 1, true);
-    is_prime[0] = is_prime[1] = false;
+/*
+    Happy Coding
+*/
 
-    for (int i = 2; i * i <= max_num; ++i) {
-        if (is_prime[i]) {
-            for (int j = i * i; j <= max_num; j += i) {
-                is_prime[j] = false;
-            }
-        }
-    }
-
-    vector<int> prefix(max_num + 1, 0);
-    for (int i = 1; i <= max_num; ++i) {
-        prefix[i] = prefix[i - 1] + (is_prime[i] ? 1 : 0);
-    }
-
-    return prefix;
-}
-
-vector<int> count_primes_in_ranges(vector<vector<int>>& queries) {
-    int max_val = 0;
-    for (auto& q : queries) {
-        max_val = max(max_val, q[1]);
-    }
-
-    vector<int> prefix = build_prime_prefix(max_val);
-
-    vector<int> result;
-    for (auto& q : queries) {
-        int L = q[0];
-        int R = q[1];
-        int count = prefix[R] - prefix[L - 1];
-        result.push_back(count);
-    }
-
-    return result;
-}
+//also find 1st and 2nd min
 void solve() {
-    vector<vector<int>> queries = { {2, 5}, {4, 7}, {1, 10} };
-    vector<int> result = count_primes_in_ranges(queries);
-    for (int val : result) {
-        cout << val << " ";
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    int min_ans = INT_MAX;
+    int min_1 = INT_MAX;
+    int min_2 = INT_MAX;
+
+    for(int i = 0;i<n;i++){
+        cin>>arr[i];
+        if (arr[i] < min_1) {
+            min_2 = min_1;
+            min_1 = arr[i];
+        } else if (arr[i] < min_2) {
+            min_2 = arr[i];
+        }
+        if(i == 0) continue;
+        int temp = arr[i]/2+arr[i-1];
+        min_ans = min(min_ans , temp);
     }
-    cout << endl;
+    int final_ans = min(min_ans,min_1+min_2);
+    cout<<final_ans<<endl;
 }
 
 int main() {
