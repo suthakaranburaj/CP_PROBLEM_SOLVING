1a7d8a1d18f639f7674ced0fe4a2303f0a850ea0
suthakaranburaj <suthakaranburaj@gmail.com>
2025-08-08 21:15:55 +0530

    Implement Stack using Arrays

--- problem.cpp
+++ problem.cpp
@@ -8,6 +8,51 @@ using namespace std;
 #define ss second
 #define all(x) (x).begin(),(x).end()
 #define sz(a) (int)(a).size()
+class ArrayStack {
+public:
+    int *arr;
+    int size;
+    int top;
+    
+    ArrayStack(int size) {
+        this->size = size;
+        this->arr = new int[size];
+        this->top = -1;
+        cout << "Stack Created Successfully!" << endl;
+    }
+
+    void push(int x) {
+        if (this->top == this->size - 1) { 
+            cout << "Stack is full" << endl;
+        } else {
+            this->top++;
+            arr[this->top] = x;
+            cout << x << " Inserted Successfully!" << endl;
+        }
+    }
+
+    int pop() {
+        if (this->top == -1) {
+            cout << "No data in stack!" << endl;
+            return -1;
+        } else {
+            return this->arr[this->top--];
+        }
+    }
+
+    int topElement() {
+        if (this->top == -1) {
+            cout << "No data in stack!" << endl;
+            return -1;
+        }
+        return this->arr[this->top];
+    }
+
+    bool isEmpty() {
+        return this->top == -1;
+    }
+};
+
 
 void print_1d_array(vector<int>&arr){
     int n = arr.size();
@@ -22,14 +67,7 @@ void print_1d_array(vector<int>&arr){
     Happy Coding
 */
 void solve() {
-    int a1, b1, a2, b2;
-    cin >> a1 >> b1 >> a2 >> b2;
-    int total = 5 * (a1 - a2) + (b1 - b2);
-    if (total < 0 || total % 6 != 0) {
-        cout << "No\n";
-    } else {
-        cout << "Yes\n";
-    }
+    cout << "Hello" << endl;
 }
 
 int main() {
@@ -40,10 +78,15 @@ int main() {
 #endif
     fastio;
     
-    int a;
-    cin>>a;
-    while(a--) {
-        solve();
-    }
+    ArrayStack st(5);
+    st.push(10);
+    st.push(20);
+    cout << "Top: " << st.topElement() << endl;
+    st.push(30);
+    cout << "Pop: " << st.pop() << endl;
+    cout << "Top after pop: " << st.topElement() << endl;
+
+    if (st.isEmpty()) cout << "Stack is empty\n";
+    else cout << "Stack is not empty\n";
     return 0;
 }
