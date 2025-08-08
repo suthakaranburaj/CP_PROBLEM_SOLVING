b7871a5176ffa6365b80e0de8ee76cb033574d69
suthakaranburaj <suthakaranburaj@gmail.com>
2025-08-08 21:56:13 +0530

    Implement Stack using Queue

--- problem.cpp
+++ problem.cpp
@@ -1,120 +1,38 @@
-/*
-    author : suthakar_09
-*/
-#include <bits/stdc++.h>
-using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define ff first
-#define ss second
-#define all(x) (x).begin(),(x).end()
-#define sz(a) (int)(a).size()
-class ArrayQueue {
-public:
-    int *arr;
-    int size;
-    int rear;
-    int front;
+class MyStack {
+private:
+    std::queue<int> q;
 
-    ArrayQueue(int size) {
-        this->size = size;
-        this->arr = new int[size];
-        this->rear = -1;
-        this->front = -1;
-        cout << "Queue Created Successfully!" << endl;
-    }
+public:
+    MyStack() {}
 
     void push(int x) {
-        if (this->rear == size - 1) { // FIXED
-            cout << "Queue is full!" << endl;
-            return;
-        } else if (this->rear == -1 && this->front == -1) {
-            this->rear = this->front = 0;
-        } else {
-            this->rear++;
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.push(q.front());
+            q.pop();
         }
-        arr[this->rear] = x;
-        cout << "Data added successfully!" << endl;
     }
 
     int pop() {
-        if (this->front == -1 && this->rear == -1) {
-            cout << "Queue is empty" << endl;
-            return -1;
-        } else if (this->front == this->rear) {
-            int val = arr[front];
-            arr[front] = -1;
-            this->front = -1;
-            this->rear = -1;
-            cout << "Data popped successfully!" << endl;
-            return val;
-        } else {
-            int val = arr[front];
-            arr[front] = -1;
-            this->front++;
-            cout << "Data popped successfully!" << endl;
-            return val;
-        }
+        int top = q.front();
+        q.pop();
+        return top;
     }
 
-    int rearElement() {
-        if (isEmpty()) {
-            cout << "Queue is empty" << endl;
-            return -1;
-        }
-        return this->arr[this->rear];
+    int top() {
+        return q.front();
     }
 
-    bool isEmpty() {
-        return this->rear == -1 && this->front == -1;
-    }
-
-    void print_queue() {
-        if (isEmpty()) {
-            cout << "Queue is empty!" << endl;
-            return;
-        }
-        for (int i = this->front; i <= this->rear; i++) {
-            cout << "Element " << i << " = " << this->arr[i] << endl;
-        }
+    bool empty() {
+        return q.empty();
     }
 };
 
-
-
-void print_1d_array(vector<int>&arr){
-    int n = arr.size();
-    cout<<"[";
-    for(int i = 0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-}
-
-/*
-    Happy Coding
-*/
-void solve() {
-    cout << "Hello" << endl;
-}
-
-int main() {
-#ifndef ONLINE_JUDGE
-    freopen("Error.txt", "w", stderr);
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    fastio;
-    
-    ArrayQueue st(5);
-    st.push(10);
-    st.push(20);
-    cout << "rear: " << st.rearElement() << endl;
-    st.push(30);
-    cout << "Pop: " << st.pop() << endl;
-    cout << "rear after pop: " << st.rearElement() << endl;
-
-    if (st.isEmpty()) cout << "Queue is empty\n";
-    else cout << "Queue is not empty\n";
-    st.print_queue();
-    return 0;
-}
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
