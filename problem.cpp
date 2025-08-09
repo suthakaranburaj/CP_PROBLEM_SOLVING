4517bfa3e27efa0675fe6881283acf2ee6466c85
suthakaranburaj <suthakaranburaj@gmail.com>
2025-08-09 17:59:21 +0530

    A - I'm a teapot

--- problem.cpp
+++ problem.cpp
@@ -1,38 +1,48 @@
-class MyStack {
-private:
-    std::queue<int> q;
+/* author : suthakar_09 */
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(x) (x).begin(),(x).end()
+#define sz(a) (int)(a).size()
 
-public:
-    MyStack() {}
+void print_1d_array(vector<int>& arr) {
+    int n = arr.size();
+    cout << "[";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "]" << endl;
+}
 
-    void push(int x) {
-        q.push(x);
-        for (int i = 0; i < q.size() - 1; i++) {
-            q.push(q.front());
-            q.pop();
+/* Happy Coding */
+void solve() {
+    cout << "Hello" << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fastio;
+
+    int N;
+    string S;
+    cin >> N >> S;
+
+    if (N >= 3) {
+        string ans = S.substr(N - 3, 3);
+        if (ans == "tea") {
+            cout << "Yes";
+        } else {
+            cout << "No";
         }
+    } else {
+        cout << "No";
     }
 
-    int pop() {
-        int top = q.front();
-        q.pop();
-        return top;
-    }
-
-    int top() {
-        return q.front();
-    }
-
-    bool empty() {
-        return q.empty();
-    }
-};
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
+    return 0;
+}
