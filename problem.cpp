e0937840052384b22cac9cbe249585819db3aea3
suthakaranburaj <suthakaranburaj@gmail.com>
2025-08-17 21:42:58 +0530

    503. Next Greater Element II

--- problem.cpp
+++ problem.cpp
@@ -8,6 +8,24 @@ using namespace std;
 #define ss second
 #define all(x) (x).begin(),(x).end()
 #define sz(a) (int)(a).size()
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        stack<int> st;         
+
+        for (int i = 0; i < 2 * n; i++) {
+            int num = nums[i % n];
+            while (!st.empty() && nums[st.top()] < num) {
+                ans[st.top()] = num;
+                st.pop();
+            }
+            if (i < n) st.push(i);
+        }
+        return ans;
+    }
+};
 
 class ArrayStack {
 public:
