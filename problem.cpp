94d5f01686d08f541207addf660920b7b8009962
suthakaranburaj <suthakaranburaj@gmail.com>
2025-10-01 20:08:57 +0530

    Expensive Buying (codechef)

--- problem.cpp
+++ problem.cpp
@@ -1,28 +1,96 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
+/*
+    author : suthakar_09
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ff first
+#define ss second
+#define all(x) (x).begin(),(x).end()
+#define sz(a) (int)(a).size()
+#include <bits/stdc++.h>
 
-    void preOrder(TreeNode* root,vector<int>&ans){
-        if(root == nullptr){
-            return;
+void swap(int* x, int* y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void print_1d_array(vector<int>&arr){
+    int n = arr.size();
+    // cout<<"[";
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+void input_1d_array(vector<int>&arr,int &n,int &k){
+    cin>>n>>k;
+    arr.resize(n);
+    for(int i = 0 ;i<n; i++){
+        cin>>arr[i];
+    }
+}
+void input_1d_array_leetcode(vector<int>&arr,int n){
+    cin>>n;
+    string s;
+    cin >> s;
+
+    int num = 0;
+    bool inNumber = false, isNegative = false;
+
+    for (char c : s) {
+        if (c == '-') {
+            isNegative = true;
+        } 
+        else if (isdigit(c)) {
+            num = num * 10 + (c - '0');
+            inNumber = true;
+        } 
+        else {
+            if (inNumber) {
+                if (isNegative) num = -num;
+                arr.push_back(num);
+                num = 0;
+                inNumber = false;
+                isNegative = false;
+            }
         }
-        ans.push_back(root->val);
-        preOrder(root->left,ans);
-        preOrder(root->right,ans);
     }
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        preOrder(root,ans);
-        return ans;
+}
+/*
+    Happy Coding
+*/
+void solve() {
+    vector<int>arr;
+    int n;
+    int k;
+    input_1d_array(arr,n,k);
+
+    sort(arr.begin(), arr.end()); 
+    reverse(arr.begin(), arr.end()); 
+    int ans = 0;
+    int i = 0;
+    while(k--){
+        ans += arr[i];
+        i++;
     }
-};
+    cout<<ans<<endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    fastio;
+    
+    int a = 1;
+    cin>>a;
+    while(a--) {
+        solve();
+    }
+    return 0;
+}
