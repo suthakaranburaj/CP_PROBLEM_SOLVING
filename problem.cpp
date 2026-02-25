beef9e162a35b6ce19278fcccd997bba6e561ca8
suthakaranburaj <suthakaranburaj@gmail.com>
2026-02-25 20:20:40 +0530

    227D Bank Glitch

--- problem.cpp
+++ problem.cpp
@@ -3,11 +3,6 @@
 */
 #include <bits/stdc++.h>
 using namespace std;
-#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define ff first
-#define ss second
-#define all(x) (x).begin(),(x).end()
-#define sz(a) (int)(a).size()
 #include <bits/stdc++.h>
 
 void swap(int* x, int* y) {
@@ -16,69 +11,20 @@ void swap(int* x, int* y) {
     *y = temp;
 }
 
-void print_1d_array(vector<int>&arr){
-    int n = arr.size();
-    // cout<<"[";
-    for(int i = 0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-}
-
-void input_1d_array(vector<int>&arr,int &n){
-    cin>>n;
-    arr.resize(n);
-    for(int i = 0 ;i<n; i++){
-        cin>>arr[i];
-    }
-}
-void input_1d_array_leetcode(vector<int>&arr,int n){
-    cin>>n;
-    string s;
-    cin >> s;
-
-    int num = 0;
-    bool inNumber = false, isNegative = false;
-
-    for (char c : s) {
-        if (c == '-') {
-            isNegative = true;
-        } 
-        else if (isdigit(c)) {
-            num = num * 10 + (c - '0');
-            inNumber = true;
-        } 
-        else {
-            if (inNumber) {
-                if (isNegative) num = -num;
-                arr.push_back(num);
-                num = 0;
-                inNumber = false;
-                isNegative = false;
-            }
-        }
-    }
-}
 /*
     Happy Coding
 */
 void solve() {
-    // vector<int>arr;
-    // int n;
-    // input_1d_array(arr,n);
-    string S,temp = "nta";
-    cin>>S;
-    if(S.size() < 3){
-        cout<<"No"<<endl;
-        return;
+    int A,B,X,Y;
+    cin>>A>>B>>X>>Y;
+
+    int ans = B;
+    while(A >= X){
+        A = A - X;
+        ans = ans + Y;
     }
-    string a = S.substr(S.size() - 3, 3);
-    if(a == temp){
-        cout<<"Yes"<<endl;
-    }
-    else{
-        cout<<"No"<<endl;
-    }
+    ans = ans + A;
+    cout<<ans<<endl;
     return;
 }
 
@@ -87,11 +33,9 @@ int main() {
     freopen("Error.txt", "w", stderr);
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-#endif
-    fastio;
-    
+#endif    
     int a = 1;
-    // cin>>a;
+    cin>>a;
     while(a--) {
         solve();
     }
